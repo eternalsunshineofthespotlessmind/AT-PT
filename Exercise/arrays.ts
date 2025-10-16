4d2bfe5073b3f2b2941edc27953fbbd9a3ed528a
eternalsunshineofthespotlessmind <irina.gutova@gmail.com>
2025-10-16 21:48:22 +0300

    Functions

--- Exercise/arrays.ts
+++ Exercise/arrays.ts
@@ -19,16 +19,33 @@ console.log(multiplyNums);
 let mapNames = arrayString.map((currName: string) => `Hello ${currName}`);
 console.log(mapNames);
 
-
-
-
-
-
 let checkTypeOf = arrayCombined.map((element: number | string) => {
   if (typeof element === 'number') {
     return element + 5;
   } else {
-    return `Welcome ${element}`
+    return `Welcome ${element}`;
   }
 });
 console.log(checkTypeOf);
+
+// method filter
+let positiveNums: number[] = [1, 2, 3, 4, 5];
+let filteredArray: number[] = positiveNums.filter(
+  (num: number) => num > 3 && num < 6);
+
+// method sort
+let sortNums: number[] = [100, 2, 30, 5, 40, 6];
+let sortedResult = sortNums.sort((a: number, b: number) => a - b);
+// console.log(sortedResult);
+
+// method push
+sortedResult.push(200);
+// console.log(sortedResult);
+
+// method pop
+sortedResult.pop();
+// console.log(sortedResult);
+
+// method shift
+sortedResult.shift();
+console.log(sortedResult);
---
+++ Exercise/functions.ts
@@ -0,0 +1,49 @@
+// Functions
+ function displayPersonalData(name: string, age: number){
+    return `${name} is ${age} years old.`
+ }
+    console.log(displayPersonalData("Joro", 32));
+
+    const personalData: string = displayPersonalData("Pete", 45);
+    console.log(personalData)
+
+    function greetingVoid(): void {
+        console.log('This function is type void')
+    };
+
+    function print(message: string): void {
+        console.log(message);
+    }
+
+    print('hello');
+
+    function divideNums (numOne: number, numTwo: number): number {
+        return numOne / numTwo;
+    }
+
+    divideNums(10, 5);
+    divideNums(150, 5);
+    divideNums(1000, 5);
+
+    console.log(divideNums(10, 5));
+
+    // arrow function
+    const sumNums = (a: number, b: number) => a + b;
+    sumNums(3,2);
+
+    // optional parameters
+    function greeting(name: string, age?: number){
+        if (age) {
+            return `${name} is ${age} years old`
+        }
+        return `Hi i am  ${name}`;
+    }
+    console.log(greeting("Todor"));
+    console.log(greeting("Trifon", 20));
+
+    // default parameters
+    function substractNums (numOne: numer = 10, numTwo: number = 5) {
+        console.log(numOne - numTwo);
+    }
+    substractNums(50);
+    substractNums(50,30);
