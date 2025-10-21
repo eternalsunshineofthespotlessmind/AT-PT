ac1bfb22932382b4dc0df1e3dba3da2707af4e97
eternalsunshineofthespotlessmind <irina.gutova@gmail.com>
2025-10-21 22:27:14 +0300

    removed unecessary files

--- Exercise/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const message = "Hello TypeScript!";
-console.log(message);
-let fName = '';
-const age = 20;
-let istrue;
-//# sourceMappingURL=index.js.map
